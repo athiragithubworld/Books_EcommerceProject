7945d27fc7638119d2e899d6afb35eb71675849c
athiragithubworld <athirahrms@gmail.com>
2023-10-30 19:11:44 +0530

    Showcase.js and css file(cover page)

---
+++ src/Components/Layouts/Showcase/Showcase.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Showcase.css";
+import { NavLink } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import SearchInputForm from "../../Forms/SearchInputForm/SearchInputForm";
+
+const Showcase = () => {
+  return (
+    <section className="showcase-container">
+      <Navbar />
+      <div className="overlay"></div>
+      <div className="showcase-content">
+        <h1>
+          Best <span className="text-primary">Books</span> Available
+        </h1>
+        <p>Buy quality books at cheap price</p>
+        <SearchInputForm />
+      </div>
+    </section>
+  );
+};
+
+export default Showcase;
