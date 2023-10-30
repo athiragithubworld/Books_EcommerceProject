89e043823b37c6bf4e3ef107fc7788de08c98644
athiragithubworld <athirahrms@gmail.com>
2023-10-30 19:06:44 +0530

    App js (Some updates)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
+import BookPage from "./Pages/BookPage/BookPage";
+import BookDetails from "./Pages/BookDetailsPage/BookDetails";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/books" element={<BookPage />} />
+        <Route path="/books-details/:id" element={<BookDetails />} />
       </Routes>
     </div>
   );
