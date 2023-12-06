5e36a72bd53fada956f629178d5ca9902d4141d5
athiragithubworld <athirahrms@gmail.com>
2023-12-06 22:16:31 +0530

    BookPage Js and css file

---
+++ src/Pages/BookPage/BookPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./BookPage.css";
+import Navbar from "../../Components/Layouts/Navbar/Navbar";
+import SearchInputForm from "../../Components/Forms/SearchInputForm/SearchInputForm";
+import ProductListingAll from "../../Components/Layouts/ProductListingAll/ProductListingAll";
+import Footer from "../../Components/Layouts/Footer/Footer";
+
+const BookPage = () => {
+  return (
+    <section>
+      {/* <h1>Book Pages</h1> */}
+      <Navbar darkTheme={true} />
+      <div className="search-container">
+        <h2>
+          Find the <span className="text-primary">Books</span> that you want
+        </h2>
+        <SearchInputForm />
+      </div>
+      <ProductListingAll />
+      <Footer />
+    </section>
+  );
+};
+
+export default BookPage;
