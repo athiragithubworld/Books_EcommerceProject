dd8d17767742977bea36ec423cf53da34527acda
athiragithubworld <athirahrms@gmail.com>
2024-01-05 22:03:09 +0530

    Navbar -Add Login&Logout Functionality

--- src/Components/Layouts/Navbar/Navbar.js
+++ src/Components/Layouts/Navbar/Navbar.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../../App";
+import { ReactComponent as Cart } from "../../../assets/CartImg.svg";
+import { getAuth, signOut } from "firebase/auth";
+import app from "../../../Firebase/Firebase";
 
-const Navbar = ({ darkTheme }) => {
+const Navbar = ({ darkTheme, darkText }) => {
+  const user = useContext(UserContext);
+
+  const auth = getAuth(app);
+  const navigate = useNavigate();
+
+  console.log(user);
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const showLoginAndSignUp = (
+    <nav className="nav-links-container">
+      <Link to="/" className={`${darkText ? "nav-links-dark" : "nav-links"}`}>
+        Home
+      </Link>
+      <Link
+        to="/books"
+        className={`${darkText ? "nav-links-dark" : "nav-links"}`}
+      >
+        Books
+      </Link>
+      <Link
+        to="/login"
+        className={`${darkText ? "nav-links-dark" : "nav-links"}`}
+      >
+        Login
+      </Link>
+      <Link
+        to="/signup"
+        className={`${darkText ? "nav-links-dark" : "nav-links"}`}
+      >
+        Sign Up
+      </Link>
+    </nav>
+  );
+
+  const showLogoutAndCart = (
+    <nav className="nav-links-container">
+      <Link to="/" className={`${darkText ? "nav-links-dark" : "nav-links"}`}>
+        Home
+      </Link>
+      <Link
+        to="/books"
+        className={`${darkText ? "nav-links-dark" : "nav-links"}`}
+      >
+        Books
+      </Link>
+      <a
+        onClick={handleLogout}
+        to="/"
+        className={`${darkText ? "nav-links-dark" : "nav-links"}`}
+      >
+        Logout
+      </a>
+      <Link to="/cart" className="cart-link">
+        <Cart />
+      </Link>
+    </nav>
+  );
+
   return (
     <section
       // className={
@@ -17,14 +89,7 @@ const Navbar = ({ darkTheme }) => {
         <a href="#" className="logo">
           Book<span className="text-primary">World</span>
         </a>
-        <nav className="nav-links-container">
-          <a href="#" className="nav-links">
-            Home
-          </a>
-          <a href="#" className="nav-links">
-            Books
-          </a>
-        </nav>
+        {user ? showLogoutAndCart : showLoginAndSignUp}
       </div>
     </section>
   );
