08953c356e1f698003ab5a0df2d8829321576e2f
athiragithubworld <athirahrms@gmail.com>
2024-01-07 22:38:49 +0530

    Some Updates

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ import BookDetails from "./Pages/BookDetailsPage/BookDetails";
 import Login from "./Pages/Login/Login";
 import SignUp from "./Pages/SignUp/SignUp";
 import CartPage from "./Pages/CartPage/CartPage";
+import ScrollToTop from "./util/ScrollToTop";
+import SearchPage from "./Pages/SearchPage/SearchPage";
 
 export const UserContext = createContext({});
+export const CartContext = createContext({});
 
 const App = () => {
   const [authenticatedUser, setAuthenticatedUser] = useState(null);
+  const [cartItems, setcartItems] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0);
 
   const auth = getAuth();
 
@@ -28,17 +33,31 @@ const App = () => {
     });
   }, []);
 
+  useEffect(() => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total = total + Number(item.price);
+    });
+
+    setTotalAmount(total);
+  }, [cartItems]);
+
   return (
-    <UserContext.Provider value={authenticatedUser}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/books" element={<BookPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/books-details/:id" element={<BookDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </UserContext.Provider>
+    <ScrollToTop>
+      <UserContext.Provider value={authenticatedUser}>
+        <CartContext.Provider value={{ cartItems, totalAmount, setcartItems }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/books" element={<BookPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/books-details/:id" element={<BookDetails />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </CartContext.Provider>
+      </UserContext.Provider>
+    </ScrollToTop>
   );
 };
 
